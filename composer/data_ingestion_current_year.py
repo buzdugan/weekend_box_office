2c19013fc9c0da2658dff8e3891098f37f058a8b
“buzdugan” <andradaorbai@yahoo.com>
2025-04-13 18:09:38 +0300

    Process files in parallel - limit 2 for testing

--- composer/data_ingestion_current_year.py
+++ composer/data_ingestion_current_year.py
@@ -1,15 +1,13 @@
-import logging
-import os
 import re
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-from airflow import DAG
+from airflow.decorators import task
 from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
+from airflow.models.dag import DAG
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 
 
@@ -42,14 +40,14 @@ SCHEMA = [
 
 
 def get_sunday_date_from_report_title(report_text):
-    """Extracts the last date from the report text and formats it as YYYY_MM_DD."""
+    "Extracts the last date from the report text and formats it as YYYY_MM_DD."
     # Replace " to " with " – "
     report_text = report_text.replace(" to ", " – ")
     # Pattern 1: "28 February – 2 March 2025 or 28 February to 2 March 2025"
     pattern1 = re.search(r"(\d{1,2})\s*(\w+)\s*–\s*(\d{1,2})\s*(\w+)\s*(\d{4})", report_text)
     # Pattern 2: "7-9 March 2025 or 7 to 9 March 2025"
     pattern2 = re.search(r"(\d{1,2})\s*–\s*(\d{1,2})\s*(\w+)\s*(\d{4})", report_text)
-
+    
     if pattern1:
         end_day, end_month, year = pattern1.group(3), pattern1.group(4), pattern1.group(5)
     elif pattern2:
@@ -60,34 +58,41 @@ def get_sunday_date_from_report_title(report_text):
     year = int(year)
     end_day = int(end_day)
     end_month = datetime.strptime(end_month, "%B").month
-    
+
     # Construct the Sunday date
     sunday_date = datetime(year, end_month, end_day).strftime("%Y_%m_%d")
     return sunday_date
 
 
-def download_current_year_reports(): 
+def extract_file_links_and_names():
     response = requests.get(URL)
     soup = BeautifulSoup(response.text, "html.parser")
     links = soup.find_all("a", href=True)
     links = [link for link in links if "Weekend box office report" in link.text]
-    
-    for link in links:
+
+    files_to_process = []
+    for link in links[1:3]:  # Limit to 2 reports for testing parallel processing
         report_title = link.text.strip()
         report_url = link["href"]
-        
         sunday_date = get_sunday_date_from_report_title(report_title)
         if sunday_date:
-            filename = f"{DATA_FOLDER}/{sunday_date}.xlsx"
-            
-            # Download the report in the original Excel format
-            report_response = requests.get(report_url)
-            with open(filename, "wb") as file:
-                file.write(report_response.content)
-                print(f"Downloaded: {filename}")
-        else:
-            logging.error(f"Report date could not be extracted for {report_title}.")
-            continue
+            files_to_process.append({
+                "url": report_url,
+                "filename": sunday_date
+            })
+            print(f"Added to list Date: {sunday_date}, URL: {report_url}")
+
+    return files_to_process
+
+
+def download_file(file_info):
+    # Download the report in the original Excel format
+    url = file_info["url"]
+    filename = f"{DATA_FOLDER}/{file_info['filename']}.xlsx"
+    response = requests.get(url)
+    with open(filename, "wb") as f:
+        f.write(response.content)
+    return file_info['filename']
 
 
 def clean_columns(df, last_sunday):
@@ -120,24 +125,33 @@ def clean_columns(df, last_sunday):
     return df
 
 
-def format_to_csv():
-    last_sunday = get_last_sunday_date()
-    # If last Sunday's report has been downloaded, convert it to parquet
-    excel_filename = f"{DATA_FOLDER}/{last_sunday}.xlsx"
-    csv_filename = f"{DATA_FOLDER}/{last_sunday}.csv"
+def convert_xlsx_to_csv(filename):
+    excel_filename = f"{DATA_FOLDER}/{filename}.xlsx"
+    csv_filename = f"{DATA_FOLDER}/{filename}.csv"
 
-    # Check if the file exists as Excel file in the home folder
-    if excel_filename:
-        # Load the data and keep only the first 15 rows
-        df = pd.read_excel(excel_filename, header=1).head(15)
-        # Clean the columns
-        df = clean_columns(df, last_sunday)
-        # Write the data to a csv file
-        df.to_csv(csv_filename, index=False)
-        print(f"Converted: {excel_filename} to csv")
-    else:
-        logging.error("Last Sunday report file not found.")
+    df = pd.read_excel(excel_filename, header=1).head(15)
+    df = clean_columns(df, filename)
+    df.to_csv(csv_filename, index=False)
+    return filename  # pass it to BQ
 
+
+def generate_bq_config(filename):
+    return {
+        "load": {
+            "sourceUris": [f"gs://{BUCKET}/data/{filename}.csv"],
+            "destinationTable": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": BIGQUERY_TABLE,
+            },
+            "schema": {"fields": SCHEMA},
+            "skipLeadingRows": 1,
+            "sourceFormat": "CSV",
+            "writeDisposition": "WRITE_APPEND",
+            "timePartitioning": {"type": "DAY", "field": "report_date"},
+            "clustering": {"fields": ["rank", "distributor", "film"]},
+        }
+    }
 
 
 
@@ -148,50 +162,41 @@ default_args = {
 }
 
 with DAG(
-    dag_id="data_ingestion_last_sunday",
-    schedule_interval=None,  # Trigger manually or as needed
-    default_args=default_args,
+    dag_id="parallel_report_processing",
+    schedule_interval=None,
     catchup=False,
     max_active_runs=1,
     tags=['dez-project'],
 ) as dag:
-    
-    start_task = EmptyOperator(task_id="start_task")
-    end_task = EmptyOperator(task_id="end_task")
 
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
 
-    download_current_year_reports_task = PythonOperator(
-        task_id="download_current_year_reports_task",
-        python_callable=download_current_year_reports,
-    )
+    @task
+    def get_files():
+        return extract_file_links_and_names()
 
+    @task
+    def download(file_info):
+        return download_file(file_info)
 
-    format_to_csv_task = PythonOperator(
-        task_id = "format_to_csv_task",
-        python_callable=format_to_csv,
-    )
+    @task
+    def to_csv(filename):
+        return convert_xlsx_to_csv(filename)
 
+    @task
+    def bq_config(filename):
+        return generate_bq_config(filename)
 
-    gcs_to_bq_task = BigQueryInsertJobOperator(
-        task_id="gcs_to_bq_task",
-        configuration={
-            "load": {
-                "sourceUris": [f"gs://{BUCKET}/data/{provide_file_name_from_loop}.csv"],
-                "destinationTable": {
-                    "projectId": PROJECT_ID,
-                    "datasetId": BIGQUERY_DATASET,
-                    "tableId": BIGQUERY_TABLE,
-                },
-                "schema": {"fields": SCHEMA},
-                "skipLeadingRows": 1,
-                "sourceFormat": "CSV",
-                "writeDisposition": "WRITE_APPEND",
-                "timePartitioning": {"type": "DAY", "field": "report_date"},
-                "clustering": {"fields": ["rank", "distributor", "film"]},
-            }
-        },
-    )
+    files = get_files()
+    downloaded = download.expand(file_info=files)
+    csv_files = to_csv.expand(filename=downloaded)
+    bq_configs = bq_config.expand(filename=csv_files)
 
+    bq_uploads = BigQueryInsertJobOperator.partial(
+        task_id="upload_to_bq",
+    ).expand(configuration=bq_configs)
 
-    start_task >> download_current_year_reports_task >> format_to_csv_task >> gcs_to_bq_task >> end_task
+    start >> files >> downloaded >> csv_files >> bq_configs >> bq_uploads >> end
+
 
