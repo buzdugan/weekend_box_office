ab5da866a742bdb856d1b5a4b4dac36767a1217e
Andrada Butnaru <andrada.butnaru@gmail.com>
2025-04-13 19:33:23 +0000

    Rename yml to schema in all folders. Add materialization in dbt_project.yml

--- dbt/models/staging/stg_movies.sql
+++ dbt/models/staging/stg_movies.sql
@@ -1,4 +1,7 @@
 -- models/staging/stg_movies.sql
-select
-  *
-from {{ source('uk_movies', 'weekend_top_15_movies') }}
+{{ log("Building stg_movies from weekend_top_15_movies", info=True) }}
+
+with source as (
+    select * from {{ source("staging", "weekend_top_15_movies") }}
+)
+select * from source
