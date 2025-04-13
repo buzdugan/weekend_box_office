2071012d45b3aaa27bc1faa399ce1acdd5808840
Andrada Butnaru <andrada.butnaru@gmail.com>
2025-04-13 19:47:07 +0000

    Remove logging

--- dbt/models/staging/stg_movies.sql
+++ dbt/models/staging/stg_movies.sql
@@ -1,5 +1,5 @@
 -- models/staging/stg_movies.sql
-{{ log("Building stg_movies from weekend_top_15_movies", info=True) }}
+
 
 with source as (
     select * from {{ source("staging", "weekend_top_15_movies") }}
